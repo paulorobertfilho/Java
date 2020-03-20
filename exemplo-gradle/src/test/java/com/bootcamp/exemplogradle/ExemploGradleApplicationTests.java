aa6b16415f72c3f1317abd3cb737ed3ed7fb166a
paulorobertfilho <paulorobert.filho@gmail.com>
2020-03-20 10:28:08 -0300

    example maven/gradle

---
+++ exemplo-gradle/src/test/java/com/bootcamp/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ exemplo-maven/src/test/java/com/bootcamp/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
